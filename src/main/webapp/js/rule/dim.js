c55e6816cc4f2cc8fde1c45e23ba1099477beb94
fornammi <qifangqian@163.com>
2017-08-14 17:56:20 +0800

    2017.8.14

--- src/main/webapp/js/rule/dim.js
+++ src/main/webapp/js/rule/dim.js
@@ -1,21 +1,26 @@
 
 
 $(function(){
-	$("#btnQuery").button().click(cuntion(){
+	$("#btnQuery").button().click(function(){
 		queryData();
 	});
-	$("#btnAdd").button().click(cuntion(){
+	$("#btnAdd").button().click(function(){
 		addDim();
 	});
+	$("#btnClose").button().click(function(){
+		addDim();
+	});
+	
 });
 
 
 function queryData(){
-	$form = $("#frmMain");
+	var $form = $("#frmMain");
 	var url = $form.attr("action");
+	console.log("dim@queryData@url:"+url);
 	$.ajax({
 		type:"POST",
-		url:url+"&method=queryData",
+		url:url+"?method=queryData",
 		data:$form.serializeArray(),
 		dataType:"html",
 		cache:false,
@@ -27,5 +32,67 @@ function queryData(){
 
 
 function addDim(){
+	var table = document.getElementById("dimTable");
 	
+	var tr = document.createElement("tr");
+	tr.name = "nameTr";
+	tr.id = "nameTr";
+	table.appendChild(tr);
+	
+	//1.dimName
+	var td1 = document.createElement("td");
+	tr.append(td1);
+	td1.name="td1";
+	td1.innerHTML = "<input type='text' name='dimName' maxlength='85' />";
+	//2.dimItem
+	var td2 = document.createElement("td");
+	tr.append(td2);
+	td2.name="td2";
+	var td2Content = "<select name='dimItemId'>" +
+			"<option value=''>---请选择---</option>" +
+			"<#list dimItemList as dimItem>" +
+			"<option value='${dimItem.dimItemId}'>${dimItem.dimItemDesc}</option>" +
+			"</#list>" +
+			"</select>";
+	td2.innerHTML = td2Content;
+	//3.operatorCompare
+	var td3 = document.createElement("td");
+	tr.append(td3);
+	td3.name="td3";
+	var td3Content = "<select name='operatorCompare'" +
+			"<option value=''>---请选择---</option>" +
+			"<#assign mapKeys = operatorCompareMap?keys>" +
+			"<#list mapKeys as operator>" +
+			"<option value='${operator}'>${operatorCompareMap[operator].desc}</option>" +
+			"</#list>" +
+			"</select>";
+	td3.innerHTML = td3Content;
+	
+	//4.dimItemValue
+	var td4 = document.createElement('td');
+	tr.appendChild(td4);
+	td4.name="td4";
+	td4.innerHTML = "<input type='text' name='dimItemValue' maxlength='128' />";
+	
+	//5.op
+	var td5 = document.createElement('td');
+	tr.appendChild(td5);	
+	var removebtn = "<a href='javascript:void(0);' onclick='preSaveDim(this);'>保存</a>";
+	var removebtn = "<a href='javascript:void(0);' onclick='removeDim(this);'>删除</a>";
+	td5.innerHTML = removebtn;
+}
+
+function removeDim(link){
+	var table = document.getElmentById("dimTable");
+	var selectedTr = link.parentElement.parentElment;// 按钮的父节点的父节点是tr
+	if(window.confirm("确认删除?")){
+		table.removeChild(selectedTr);
+	}
+}
+
+
+function preSaveDim(link){
+	var selectTr = link.parentElement.parentElment;
+	var dimName = selectTr.cell[0].childNodes[0].value;
+	console.log("preSaveDim@dimName="+dimName);
 }
--- src/test/java/redis/RedisTemplateHelper.java
+++ src/test/java/redis/RedisTemplateHelper.java
@@ -34,18 +34,26 @@ public class RedisTemplateHelper {
 		ApplicationContext ac = new FileSystemXmlApplicationContext(new String[]{
 						"classpath*:redis/redis_dev.xml"
 				});
-		//RedisTemplate<String, Object> redisTemplate = (RedisTemplate<String, Object>)ac.getBean("strObjRedisTemplate");
-		RedisTemplate<String, MamCustomer> redisTemplate = (RedisTemplate<String, MamCustomer>)ac.getBean("strHashRedisTemplate");
+		//RedisTemplate<String, Object> redisObjTemplate = (RedisTemplate<String, Object>)ac.getBean("strObjRedisTemplate");
+		RedisTemplate<String, Object> redisHashObjTemplate1 = (RedisTemplate<String, Object>)ac.getBean("strHashRedisTemplate");
+		String mailKey = "noticeType";
+		//设置值
+		//redisHashObjTemplate1.opsForValue().set(mailKey, 1);
+		System.out.println("curMailNo:key="+mailKey+"|value="+redisHashObjTemplate1.opsForValue().get(mailKey));
+		System.out.println("nextMailNo:key="+mailKey+"|value="+genMailNo(redisHashObjTemplate1, mailKey));
+		System.out.println("aftergenMailNo, curMailNo:key="+mailKey+"|value="+redisHashObjTemplate1.opsForValue().get(mailKey));
+		
+		/*RedisTemplate<String, MamCustomer> redisTemplate = (RedisTemplate<String, MamCustomer>)ac.getBean("strHashRedisTemplate");
 		//String objKey = CACHE_KEY_RATEDATA_MERCHANT_TXN + "10012077336";
 		String objKey = CACHE_KEY_MEMBER_INFO + "10012245533";
 		System.out.println("objKey="+objKey);
 		boolean isExists = redisTemplate.hasKey(objKey);
 		System.out.println("[KEY="+objKey+"] isExists="+isExists);
 		if(isExists){
-			/**
+			*//**
 			 * Test.1>>>单个值
-			 */
-			/**
+			 *//*
+			*//**
 			 * 注意：model的package路径，必须与set时的model路径一致,否则抛以下异常
 			 * Exception in thread "main" org.springframework.data.redis.serializer.SerializationException: 
 			 * com.bill99.rm.skeye.aba.model.MamCustomer; nested exception is java.lang.RuntimeException: 
@@ -55,17 +63,29 @@ public class RedisTemplateHelper {
 			 * at com.bill99.inf.redis.ext.serializer.JdkSerializer.deserialize(JdkSerializer.java:14)
 			 * ... 7 more
 			 * Caused by: java.lang.ClassNotFoundException: com.bill99.rm.skeye.aba.model.MamCustomer
-			 */
+			 *//*
 			MamCustomer objVal = redisTemplate.opsForValue().get(objKey);
 			JSONObject jsonObj = JSONObject.fromObject(objVal);
 			System.out.println("objVal="+jsonObj.toString());
-			/**
+			*//**
 			 * Test.2>>>List
-			 */
+			 *//*
 			//queryList(redisTemplate, objKey);
-		}
+		}*/
 		
 	}
+	
+	
+	public static String genMailNo(RedisTemplate<String, Object> redisTemplate, String key){
+		String returnStr = "";
+		int curNo = (Integer)redisTemplate.opsForValue().get(key);
+		int nextNo = curNo + 1;
+		//更新缓存
+		redisTemplate.opsForValue().set(key, nextNo);
+		returnStr = RedisTemplateHelper.formatNumWithZero(nextNo, 2);
+		return returnStr;
+	}
+	
 	
 	public static List<?> queryList(RedisTemplate<String, Object> redisTemplate, String key){
 		List resultList = new ArrayList();
@@ -85,5 +105,20 @@ public class RedisTemplateHelper {
 			}
 		}*/
 		return resultList;
+	}
+	
+	
+	/**
+	 * 数字前补零
+	 * 最高效简洁的方法：String.format
+	 * @param curNum-当前数字
+	 * @param maxLen-最终长度
+	 * @return
+	 */
+	public static String formatNumWithZero(int curNum, int maxLen){
+		// 0 代表前面补充0         
+	    // d代表参数为正数型 
+		String newStr = String.format("%0"+maxLen+"d", curNum);
+		return newStr;
 	}
 }
